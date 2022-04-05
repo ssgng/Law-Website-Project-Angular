749621400b24cbbc70ba44625519c5103a64a891
keremozbakir <63881378+keremozbakir@users.noreply.github.com>
2022-04-05 02:57:05 +0200

    slider bug fixed

--- hukuki/src/app/app.module.ts
+++ hukuki/src/app/app.module.ts
@@ -1,18 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+//import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
+import { CarouselModule } from 'ngx-owl-carousel-o';
+import { RouterModule } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+ 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent 
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CarouselModule,
+    RouterModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- hukuki/src/app/components/home/home.component.ts
+++ hukuki/src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { OwlOptions } from 'ngx-owl-carousel-o';
 
 @Component({
   selector: 'app-home',
@@ -11,5 +12,34 @@ export class HomeComponent implements OnInit {
 
   ngOnInit(): void {
   }
+  slidesStore = [
+    { "src": "../../../assets/img/banner-1.png", "title": "this is title","id":"1" },
+    { "src": "../../../assets/img/banner-2.png", "title": "this is titles4" ,"id":"2" },
+    { "src": "../../../assets/img/banner-1.png","title":"this is title3","id":"3"  }
+  
+  ]
+  
+
+  customOptions: OwlOptions = {
+    autoHeight: true,
+ 
+    loop: true,
+    mouseDrag: true,
+    touchDrag: true,
+    pullDrag: true,
+    dots: true,
+    navSpeed: 700,
+    navText: [ '<i class="fa-chevron-left"></i>', '<i class="fa-chevron-right></i>"' ],
+    autoplay:true,
+     
+    responsive: {
+      0: {
+        items: 1
+      } 
+    },
+    nav: true,
+    
+  }
+
 
 }
