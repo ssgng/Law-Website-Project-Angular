f20ee4d20a4852fbf311a04a225a42ffa23a3e85
keremozbakir <63881378+keremozbakir@users.noreply.github.com>
2022-11-12 01:24:33 +0100

    title set added

--- hukuki/src/app/app.component.ts
+++ hukuki/src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Title, Meta } from '@angular/platform-browser';  
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'hukuki';
+  constructor(private titleService: Title, private metaTagService: Meta) { }
+  
+  ngOnInit(): void {
+    this.titleService.setTitle("Safa Hukuk");  
+    
+    this.metaTagService.addTags([
+      { name: 'keywords', content: 'Safa Hukuk' },  
+      { name: 'Safa Hukuk', content: 'index, follow' }, 
+      { charset: 'UTF-8' }  
+    ]);  
+
+  }
 }
